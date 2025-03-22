9988901f96ed3ccf06a3c5bebdaa04eb0b42aa71
nitinsandiego <nitinsandiego@gmail.com>
2025-03-21 18:46:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import os
 from flask import Flask, jsonify
 from util import ExcelToJsonUtil
 
 app = Flask(__name__)
-
-# Get the absolute path of the current script (main.py)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def home():
@@ -17,13 +13,11 @@ def get_info():
 
 @app.route("/compsci")
 def get_compsci():
-    file_path = os.path.join(BASE_DIR, "data", "CompScienceRanking.xlsx")
-    return ExcelToJsonUtil.convert_to_json(file_path)
+    return ExcelToJsonUtil.convert_to_json("data/CompScienceRanking.xlsx")
 
 @app.route("/national")
 def get_national():
-    file_path = os.path.join(BASE_DIR, "data", "NationalUnivRanking.xlsx")
-    return ExcelToJsonUtil.convert_to_json(file_path)
+    return ExcelToJsonUtil.convert_to_json("data/NationalUnivRanking.xlsx")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
